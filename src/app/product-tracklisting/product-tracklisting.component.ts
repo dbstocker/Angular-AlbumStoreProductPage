4c75421ace86b6bee911494b491187c83f86af78
dbstocker <dstocker@notesettersinc.com>
2019-09-18 09:32:35 -0400

    part5 done

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ProductService } from '../product.service';
 import { Album } from '../album';
+import { ProductService } from '../product.service';
 
 @Component({
-  selector: 'app-product-description',
-  templateUrl: './product-description.component.html',
-  styleUrls: ['./product-description.component.css']
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
 })
-export class ProductDescriptionComponent implements OnInit {
+export class ProductTracklistingComponent implements OnInit {
   albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
@@ -16,6 +16,7 @@ export class ProductDescriptionComponent implements OnInit {
   ngOnInit() {
     this._productService.getAlbum(1)
       .subscribe(response => this.albumInfo = response);
+
   }
 
 }
